916d704ff755adbd5927363a3d48bba639fdd4c6
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-03-25 11:38:55 +0800

    0325 Update

--- week1/Dog.java
+++ week1/Dog.java
@@ -1,5 +1,55 @@
 public class Dog {
-    public static void bark(){
-        System.out.println("Barking...");
+    public int weight;
+    public static String species="Dogs";
+
+    public Dog(int weight) {//Constructor
+        this.weight = weight;
+    }
+
+    public static Dog max(Dog d1, Dog d2){
+        if (d1.weight > d2.weight) {
+            return d1;
+        }
+        return d2;
+    }
+
+    public void bark(){//Non-static method
+        if(weight < 10){
+            System.out.println("Small Barking...");
+        }
+        else if(weight < 30){
+            System.out.println("Middle Barking...");
+        }
+        else{
+            System.out.println("Huge Barking...");
+        }
+    }
+    static int max(int x,int y){
+        if(x > y){
+            return x;
+        }
+        return y;
+    }
+    static int min(int x, int y){
+        return -max(-x,-y);
+    }
+    public static void largerThanFourNeighbors(Dog[] dogs){
+        int n=dogs.length;
+        boolean isLarge=true;
+
+        for(int i = 0; i < n; i++){
+            isLarge=true;
+
+            for(int j=max(i-2,0);j<min(n,i+2);j++){
+                if(dogs[i].weight<dogs[j].weight){
+                    isLarge=false;
+                    break;
+                }
+            }
+
+            if(isLarge){
+                System.out.println(dogs[i].weight);
+            }
+        }
     }
 }
--- week1/DogLauncher.java
+++ week1/DogLauncher.java
@@ -1,6 +1,49 @@
+/*
+Lec2
+
+instead of making classes
+like dog1 dog2 dog...,
+
+the difference of static/instant
+static cannot access instant var
+invoke static methods/var using class name
+invoke instant methods/var using instance name
+
+why all in classes and static?
+offer fewer ways to do things
+
+helper methods
+goal - write a method that finds int that
+bigger than all 2 left and 2 right neighbors
+if neighbor not exist, just consider the existing ones.
+array
+-> function of find: for each, judge
+-> function of judge
+
+about "this" - adding this when initialized recommended
+*/
+
 public class DogLauncher {
+
     public static void main(String[] args) {
-        Dog.bark();
-        Dog2.bark();
+        Dog smallDog = new Dog(5);//Declaration, Instantiation, Assignment
+        Dog bigDog = new Dog(100);
+        Dog middleDog = new Dog(20);
+        smallDog.bark();
+        middleDog.bark();
+        bigDog.bark();
+
+        Dog[] dogs = {new Dog(1), bigDog, middleDog};
+        for(int i=0;i<dogs.length;i++){
+            dogs[i].bark();
+        }
+        Dog.max(smallDog, middleDog).bark();
+        System.out.println();
+        int[] sizes={10, 20, 30, 25, 20, 40, 10};
+        Dog[] dogsLTFN = new Dog[sizes.length];
+        for(int i=0;i<sizes.length;i++){
+            dogsLTFN[i] = new Dog(sizes[i]);
+        }
+        Dog.largerThanFourNeighbors(dogsLTFN);
     }
 }
