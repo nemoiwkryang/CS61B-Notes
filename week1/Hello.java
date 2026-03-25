5dffe611b1f2aafb1ad1725a75b6d8178df0c3b7
nemoiwkryang <yanggr2025@shanghaitech.edu.cn>
2026-03-25 08:24:12 +0800

    0324Update

---
+++ week1/Hello.java
@@ -0,0 +1,15 @@
+/**
+java --javac--> class --java--> interpreted
+var & loop like cpp
+integer /float number/ string +string -> string
+all code is part of a class
+
+about coding style
+
+javadoc
+ */
+public class Hello{
+    public static void main(String args[]){
+        System.out.println('\b'+"114514");
+    }
+}
